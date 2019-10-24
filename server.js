fac2deb6082c276d4d274a060751dc9477a382d0
Glitch (hello-webpage) <none>
2019-10-24 20:36:15 +0000

    💡🏑 Checkpoint ./server.js:3755719/2187 ./package.json:3755719/1312

--- server.js
+++ server.js
@@ -4,10 +4,31 @@ const app = express();
 app.use(express.static("public"));
 
 const listener = app.listen(process.env.PORT, function() {
-  console.log("Listening on port " + listener.address().port)
-})
+  console.log("Listening on port " + listener.address().port);
+});
 
 //TWITTER
+var Twitter = require("twitter");
+var T = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
-var latestTweets = require('latest-tweets')
-latestTweets('sw_help',function(null,tweets))
+var params = {
+  q: "from:sw_help",
+  count: 1,
+  result_type: "recent",
+  lang: "en"
+};
+
+T.get("search/tweets", params, function(err, data, response) {
+  if (!err) {
+    //do stuff
+    console.log(data);
+    console.log(response);
+  } else {
+    console.log(err);
+  }
+});
